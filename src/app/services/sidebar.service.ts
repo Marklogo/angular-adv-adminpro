9ef613e39cc16c0b1ed83cb00c768d232e1a858a
MarkLoGo <marklogo@gmail.com>
2020-09-05 13:23:45 +0200

    Fin seccion 8

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -11,7 +11,9 @@ export class SidebarService {
       submenu: [
         { titulo: 'Main', url:'/' },
         { titulo: 'ProgressBar', url:'/dashboard/progress' },
-        { titulo: 'Graficas', url:'/dashboard/grafica1' }
+        { titulo: 'Graficas', url:'/dashboard/grafica1' },
+        { titulo: 'Promesas', url:'/dashboard/promesas' },
+        { titulo: 'Rxjs', url:'/dashboard/rxjs' }
       ],
     },
   ];
--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,11 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy {
 
-  constructor() { }
+  public pageTitle;
+  public tituloSubs$: Subscription;
 
-  ngOnInit(): void {
+  
+  constructor(private router: Router) {
+    this.tituloSubs$ = this.getArgumentosRuta()
+    .subscribe(title => {
+      this.pageTitle=title
+      document.title=`Admin-Pro - ${title}` 
+    });
+  }
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
   }
 
+
+  getArgumentosRuta() {
+    return this.router.events
+    .pipe(
+      filter(event => event instanceof ActivationEnd),
+      filter((event:ActivationEnd) => event.snapshot.firstChild === null ),
+      map( event =>  event.snapshot.data['titulo'])
+    )
+  }
 }
